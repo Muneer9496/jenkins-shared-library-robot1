d745edea936a335df4ced0cae59fd669ec0e9689
Muneer <muneersurya@gmail.com>
2023-04-14 16:03:30 +0530

    git auto push

--- vars/python.groovy
+++ vars/python.groovy
@@ -10,52 +10,52 @@ def call() {
     }
 }
 
-// Call is the default function which will be called when you call the fileName
-// **** Uncomment it only when you prefer to use Declarative Pipeline 
-// def call() {
-//     pipeline {
-//         agent any 
-//         environment { 
-//             SONAR = credentials('SONAR') 
-//             SONAR_URL = "172.31.9.236"
-//         }
-//         stages {
-//             stage('Lint Checks') {
-//                 steps {
-//                     script {
-//                         lintChecks()
-//                     }
-//                 }
-//             }
-//             stage('Sonar Checks') {
-//                 steps {
-//                     script {
-//                         env.ARGS="-Dsonar.sources=."
-//                         common.sonarChecks()
-//                     }
-//                 }
-//             }
-//         stage('Test Cases') {
-//             parallel {
-//                 stage('Unit Testing') {
-//                     steps {
-//                         // sh "py test"
-//                         sh "echo Performing Unit Testing"
-//                     }
-//                 }
-//                 stage('Integration Testing') {
-//                     steps {
-//                         // sh "py verify"
-//                         sh "echo Integration Unit Testing"
-//                     }
-//                 }
-//                 stage('Functional Testing') {
-//                     steps {
-//                         sh "echo Integration Unit Testing"
-//                         }
-//                     }
-//                 }
-//             }
-//         }
-//     }
-// }
+Call is the default function which will be called when you call the fileName
+**** Uncomment it only when you prefer to use Declarative Pipeline 
+def call() {
+    pipeline {
+        agent any 
+        environment { 
+            SONAR = credentials('SONAR') 
+            SONAR_URL = "172.31.9.236"
+        }
+        stages {
+            stage('Lint Checks') {
+                steps {
+                    script {
+                        lintChecks()
+                    }
+                }
+            }
+            stage('Sonar Checks') {
+                steps {
+                    script {
+                        env.ARGS="-Dsonar.sources=."
+                        common.sonarChecks()
+                    }
+                }
+            }
+        stage('Test Cases') {
+            parallel {
+                stage('Unit Testing') {
+                    steps {
+                        // sh "py test"
+                        sh "echo Performing Unit Testing"
+                    }
+                }
+                stage('Integration Testing') {
+                    steps {
+                        // sh "py verify"
+                        sh "echo Integration Unit Testing"
+                    }
+                }
+                stage('Functional Testing') {
+                    steps {
+                        sh "echo Integration Unit Testing"
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
